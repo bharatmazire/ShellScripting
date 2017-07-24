dcdec974d6a7bf00fd13d6006c45b8ec1b3c59b9
Bharat Mazire <mazire.bharat@gmail.com>
2017-07-25 01:12:34 +0530

    Update CountOfNonVowel.sh

--- CountOfNonVowel.sh
+++ CountOfNonVowel.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+# for execution : sh <file-name.sh> <file name to open and check for non vowel words>
+#               : chmod 744 <file-name.sh>
+#               : ./<file-name.sh> <file name to open and check for non vowel words>
+#
 echo "Welcome"
 echo "Entered File Is : "$1
 cat $1 | tr " " "\n" > SampleCountOfNonVowel
