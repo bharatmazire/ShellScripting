822ebeb37f52d6025f0de2ec99f8a1bec7d55dcb
Bharat Mazire <mazire.bharat@gmail.com>
2017-07-25 11:30:53 +0530

    Update PrintMyNameCommandLine.sh

--- PrintMyNameCommandLine.sh
+++ PrintMyNameCommandLine.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
+# for execution : $ sh <file-name.sh> <name to print>
+#               or
+#               : $ chmod 744 <file-name.sh>
+#               : $ ./<file-name.sh> <name to print>
 echo "Welcome "$1
